8ee5e51553638e3a3edc8014775c656667aab3e4
Iswenzz <suxlolz1528@gmail.com>
2020-07-21 13:46:35 +0200

    [E7] added leaflet map

--- courses/exo7/main.js
+++ courses/exo7/main.js
@@ -1,3 +1,9 @@
+const map = L.map("person-location");
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright%22%3EOpenStreetMap</a> contributors'
+}).addTo(map);
+let marker = null;
+
 const randomizePerson = () =>
 {
 	fetch("https://randomuser.me/api/").then(res => res.json()).then(j => 
@@ -5,8 +11,6 @@ const randomizePerson = () =>
 		const person = j.results[0];
 		if (person)
 		{
-			console.log(person);
-			
 			document.getElementById("person-picture").setAttribute("src", person.picture.medium);
 			document.getElementById("person-name").innerText = `${person.name.first} ${person.name.last}`;
 
@@ -17,6 +21,11 @@ const randomizePerson = () =>
 			const date = document.getElementById("person-birthday");
 			date.setAttribute("datetime", person.dob.date);
 			date.innerText = new Date(person.dob.date).toLocaleDateString();
+			
+			map.setView([person.location.coordinates.latitude, person.location.coordinates.longitude], 5);
+			if (marker) 
+				map.removeLayer(marker);
+			marker = L.marker([person.location.coordinates.latitude, person.location.coordinates.longitude]).addTo(map);
 		}
 	});
 }
